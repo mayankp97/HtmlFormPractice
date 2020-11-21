265b9f0475255cc42ee3c3a3636eca41a37035c2
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-11-21 09:58:15 +0530

    [Mayank] Add validation for email

--- Js/SimpleForm.js
+++ Js/SimpleForm.js
@@ -1,3 +1,11 @@
+const salary=document.querySelector('#salary');
+const output=document.querySelector('.salary-output');
+output.textContent=salary.value;
+salary.addEventListener('input',function(){
+output.textContent=salary.value;
+})
+
+//UC1 : Validate Name
 const text=document.querySelector('#text');
 const textError=document.querySelector('.text-error');
 text.addEventListener('input', function(){
@@ -6,11 +14,13 @@ if(namRegex.test(text.value))
     textError.textContent="";
 else
     textError.textContent="Name is incorrect";
-})
+});
  
-const salary=document.querySelector('#salary');
-const output=document.querySelector('.salary-output');
-output.textContent=salary.value;
-salary.addEventListener('input',function(){
-output.textContent=salary.value;
-})
+//UC2 : Validate Email
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input',() => textError.textContent = 
+                        new RegExp('^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$').test(email) ?
+                        '' : 'Email is Incorrect')
+                        
+
